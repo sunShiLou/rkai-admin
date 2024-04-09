95995f8594ea1fd23c9a06d0415aec111e92252f
sundiancai <921632301@qq.com>
2024-04-09 22:41:37 +0800

    初始化

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+console.log("0000")
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+
+
+    </>
+  )
+}
+
+export default App
